46c10aaf5eb4990436340c5206015769dc862db9
FlySheep <435242634@qq.com>
2017-03-21 20:00:52 +0800

    关闭 banner

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,18 @@
 package com.example;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+
+        // 关闭 banner
+        SpringApplication springApplication = new SpringApplication(DemoApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
+    }
 }
