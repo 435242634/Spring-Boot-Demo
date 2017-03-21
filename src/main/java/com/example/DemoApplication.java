10eb20faf924a1044507f420c59f2a1768b8df2c
FlySheep <435242634@qq.com>
2017-03-21 20:39:10 +0800

    常规属性的配置

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -14,5 +18,16 @@ public class DemoApplication {
         SpringApplication springApplication = new SpringApplication(DemoApplication.class);
         springApplication.setBannerMode(Banner.Mode.OFF);
         springApplication.run(args);
+    }
+
+    @Value("${book.name}")
+    private String bookName;
+
+    @Value("${book.price}")
+    private double bookPrice;
+
+    @RequestMapping("/")
+    public String index() {
+        return bookName + bookPrice;
     }
 }
