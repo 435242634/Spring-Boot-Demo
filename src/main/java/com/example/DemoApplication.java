301996f2346399612dffa0cd28ba7e8fb88c5caf
FlySheep <435242634@qq.com>
2017-03-21 08:49:30 +0800

    编写 Hello World

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController // 为了方便，没有新建控制器类，而是直接在入口类中编写代码
+@SpringBootApplication  // Spring Boot 项目的核心注解，主要目的是开启自动配置
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
