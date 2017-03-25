7153e9ac55682eae64b4c499cf1a1785eb02108e
FlySheep <435242634@qq.com>
2017-03-25 09:18:14 +0800

    重构了项目

---
+++ src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,68 @@
+package com.example.controller;
+
+import com.example.dao.PersonMapper;
+import com.example.domain.PersonDO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by FlySheep on 17/3/25.
+ */
+@EnableTransactionManagement  // 需要事务的时候加上
+@RestController
+public class PersonController {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @RequestMapping("/save")
+    public Integer save() {
+        PersonDO personDO = new PersonDO();
+        personDO.setName("张三");
+        personDO.setAge(18);
+        personMapper.insert(personDO);
+        return personDO.getId();
+    }
+
+    @RequestMapping("/update")
+    public Long update() {
+        PersonDO personDO = new PersonDO();
+        personDO.setId(2);
+        personDO.setName("旺旺");
+        personDO.setAge(12);
+        return personMapper.update(personDO);
+    }
+
+    @RequestMapping("/delete")
+    public Long delete() {
+        return personMapper.delete(11L);
+    }
+
+    @RequestMapping("/selectById")
+    public PersonDO selectById() {
+        return personMapper.selectById(2L);
+    }
+
+    @RequestMapping("/selectAll")
+    public List<PersonDO> selectAll() {
+        return personMapper.selectAll();
+    }
+
+    @RequestMapping("/transaction")
+    @Transactional  // 需要事务的时候加上
+    public Boolean transaction() {
+        delete();
+
+        int i = 3 / 0;
+
+        save();
+
+        return true;
+    }
+
+}
--- src/main/java/com/example/dao/PersonMapper.java
+++ src/main/java/com/example/dao/PersonMapper.java
@@ -22,19 +22,47 @@ public interface PersonMapper {
      * http://www.mybatis.org/mybatis-3/zh/java-api.html
      */
 
+    /**
+     * 添加操作，返回新增元素的 ID
+     *
+     * @param personDO
+     */
     @Insert("insert into person(name,age) values(#{name},#{age})")
     @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
     void insert(PersonDO personDO);
 
+    /**
+     * 更新操作
+     *
+     * @param personDO
+     * @return 受影响的行数
+     */
     @Update("update person set name=#{name},age=#{age} where id=#{id}")
     Long update(PersonDO personDO);
 
+    /**
+     * 删除操作
+     *
+     * @param id
+     * @return 受影响的行数
+     */
     @Delete("delete from person where id=#{id}")
     Long delete(@Param("id") Long id);
 
+    /**
+     * 查询所有
+     *
+     * @return
+     */
     @Select("select id,name,age from person")
     List<PersonDO> selectAll();
 
+    /**
+     * 根据主键查询单个
+     *
+     * @param id
+     * @return
+     */
     @Select("select id,name,age from person where id=#{id}")
     PersonDO selectById(@Param("id") Long id);
 }
